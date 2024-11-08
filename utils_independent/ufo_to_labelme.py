aa4b4955e3b364b0651bf95dc200034197bde1ce
MICHAA4 <sycha2000@gmail.com>
2024-11-08 10:23:55 +0900

    [refactor] main 함수 추가

--- utils_independent/ufo_to_labelme.py
+++ utils_independent/ufo_to_labelme.py
@@ -42,11 +42,15 @@ def ufo_to_labelme(ufo_json_path, output_dir):
 
     print("Conversion completed. LabelMe JSON files saved in:", output_dir)
 
-# UFO 파일 경로와 결과를 저장할 디렉토리 설정
-languages = ['chinese', 'japanese', 'thai', 'vietnamese']
 
-for language in languages:
-    ufo_file_path = f'code/data/{language}_receipt/ufo/train.json'
-    output_dir = f'labelme_jsons/{language}_receipt'
-    os.makedirs(output_dir, exist_ok=True)
+def main():
+    languages = ['chinese', 'japanese', 'thai', 'vietnamese']
+
+    for language in languages:
+        ufo_file_path = f'code/data/{language}_receipt/ufo/train.json'
+        output_dir = f'labelme_jsons/{language}_receipt'
+        os.makedirs(output_dir, exist_ok=True)
     ufo_to_labelme(ufo_file_path, output_dir)
+
+if __name__ == '__main__':
+    main()
