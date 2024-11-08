40baefb88fd2b62dd2ba729dc5e8cae2665cd214
yeyechu <yeyechuuu@gmail.com>
2024-11-08 01:55:27 +0000

    [feat] 파일 병합

--- utils_independent/ufo_to_labelme.py
+++ utils_independent/ufo_to_labelme.py
@@ -1,7 +1,8 @@
-import os
 import json
+import os
+import shutil
 
-def ufo_to_labelme(ufo_json_path, output_dir):
+def ufo_to_labelme(ufo_json_path, image_dir, output_dir):
     with open(ufo_json_path, 'r', encoding='utf-8') as f:
         ufo_data = json.load(f)
 
@@ -20,33 +21,40 @@ def ufo_to_labelme(ufo_json_path, output_dir):
         for word_id, word_data in image_data["words"].items():
             transcription = word_data["transcription"]
             points = word_data["points"]
-            
-            # transcription = null인 경우 처리
-            if transcription == None:
+
+            # transcription이 null인 경우 처리
+            if transcription is None:
                 transcription = "unknown"
 
-            # LabelMe의 shape
+            # LabelMe 형식으로 shape 생성
             shape = {
                 "label": transcription,
                 "points": points,
                 "group_id": None,
-                "shape_type": "polygon", # rectangular가 아닌 polygon으로 설정
+                "shape_type": "polygon",
                 "flags": {}
             }
             labelme_data["shapes"].append(shape)
 
-        # LabelMe JSON으로 저장
+        
         labelme_json_path = os.path.join(output_dir, os.path.splitext(image_filename)[0] + '.json')
         with open(labelme_json_path, 'w', encoding='utf-8') as f:
             json.dump(labelme_data, f, ensure_ascii=False, indent=4)
 
-    print("Conversion completed. LabelMe JSON files saved in:", output_dir)
+        
+        source_image_path = os.path.join(image_dir, image_filename)
+        target_image_path = os.path.join(output_dir, image_filename)
+        if os.path.exists(source_image_path):
+            shutil.copy(source_image_path, target_image_path)
 
-# UFO 파일 경로와 결과를 저장할 디렉토리 설정
+    print("Conversion completed. LabelMe JSON files and images saved in:", output_dir)
+
+
 languages = ['chinese', 'japanese', 'thai', 'vietnamese']
 
 for language in languages:
-    ufo_file_path = f'code/data/{language}_receipt/ufo/train.json'
+    ufo_file_path = f'../code/data_fixed_bbox/{language}_receipt/ufo/train.json'
+    image_dir = f'../code/data_fixed_bbox/{language}_receipt/img/train' 
     output_dir = f'labelme_jsons/{language}_receipt'
     os.makedirs(output_dir, exist_ok=True)
-    ufo_to_labelme(ufo_file_path, output_dir)
+    ufo_to_labelme(ufo_file_path, image_dir, output_dir)
